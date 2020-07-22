ddda0ffd8f768adef20b2894273733caddee37d9
Viraj Puri <vp1@fb.com>
2020-07-22 13:52:44 -0400

    Got camera2 API installed, and a good amount working so that when i click on taking a picture, the camera app loads

--- app/src/main/java/com/example/groomzoom/GalleryActivity.java
+++ app/src/main/java/com/example/groomzoom/GalleryActivity.java
@@ -2,7 +2,9 @@ package com.example.groomzoom;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageButton;
 
 public class GalleryActivity extends AppCompatActivity {
@@ -19,5 +21,17 @@ public class GalleryActivity extends AppCompatActivity {
         btnFrontPic = (ImageButton) findViewById(R.id.btnFrontPic);
         btnLeftPic = (ImageButton) findViewById(R.id.btnLeftPic);
         btnRightPic = (ImageButton) findViewById(R.id.btnRightPic);
+        btnFrontPic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchCameraActivity();
+            }
+        });
     }
+
+    private void launchCameraActivity() {
+        Intent changePictures = new Intent(getApplicationContext(), CustomCamera.class);
+        startActivity(changePictures);
+    }
+
 }
