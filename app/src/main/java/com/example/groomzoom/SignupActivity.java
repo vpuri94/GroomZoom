b5f2804731cf8049af2897e4ff5c0c1fc32815e9
Viraj Puri <vp1@fb.com>
2020-08-03 14:55:42 -0400

    finished implementing detailed signup screen

--- app/src/main/java/com/example/groomzoom/SignupActivity.java
+++ app/src/main/java/com/example/groomzoom/SignupActivity.java
@@ -13,6 +13,9 @@ import android.widget.EditText;
 import com.parse.ParseFile;
 import com.parse.ParseUser;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import es.dmoral.toasty.Toasty;
 
 public class SignupActivity extends AppCompatActivity {
@@ -27,8 +30,23 @@ public class SignupActivity extends AppCompatActivity {
     private CheckBox cbWax;
     private CheckBox cbBlowdry;
     public static final int requestCodeNum = 999;
+    public static final int requestCodeNum2 = 1000;
     String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
     ParseUser currentUser = ParseUser.getCurrentUser();
+    private String servicesKey = "services";
+    private String addressKey = "address";
+    private static final int RESULT_OK = 23;
+    private String signupSuccess = "Signed up successfully!";
+    String barberKey = "barber";
+    String noEmailMsg = "First input an email address!";
+    String invalidEmailMsg = "Invalid email address!";
+    String invalidPriceMsg = "First input a price";
+    String noAddressMsg = "First input a postal address!";
+    String serviceMsg = "Must Select atleast one service!";
+    String emailKey = "email";
+    String priceKey = "price";
+    String geokey = "mapPoint";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,11 +55,11 @@ public class SignupActivity extends AppCompatActivity {
         etEmail = (EditText) findViewById(R.id.etEmailAddress);
         btnAddress = (Button) findViewById(R.id.btnAddress);
         etPrice = (EditText) findViewById(R.id.editTextNumber);
-        cbHaircut = findViewById(R.id.cbHaircut);
-        cbBeard = findViewById(R.id.cbBeard);
-        cbColor = findViewById(R.id.cbColor);
-        cbWax = findViewById(R.id.cbWax);
-        cbBlowdry = findViewById(R.id.cbBlowdry);
+        cbHaircut = (CheckBox) findViewById(R.id.cbHaircut2);
+        cbBeard = (CheckBox) findViewById(R.id.cbBeard2);
+        cbColor = (CheckBox)findViewById(R.id.cbColor2);
+        cbWax = (CheckBox)findViewById(R.id.cbWax2);
+        cbBlowdry = (CheckBox) findViewById(R.id.cbBlowdry2);
         btnPictures = findViewById(R.id.btnPictures);
 
         btnAddress.setOnClickListener(new View.OnClickListener() {
@@ -51,34 +69,39 @@ public class SignupActivity extends AppCompatActivity {
             }
         });
 
-        if(!currentUser.getBoolean("barber"))
+        if(!currentUser.getBoolean(barberKey))
             etPrice.setVisibility(View.GONE);
 
         btnPictures.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if(etEmail.getText().toString().isEmpty()){
-                    Toasty.error(getApplicationContext(), "First input an email address!", Toasty.LENGTH_SHORT, true).show();
+                    Toasty.error(getApplicationContext(), noEmailMsg, Toasty.LENGTH_SHORT, true).show();
                     return;
                 }
                 if(!etEmail.getText().toString().trim().matches(emailPattern)){
-                    Toasty.error(getApplicationContext(), "Invalid email address!", Toasty.LENGTH_SHORT, true).show();
+                    Toasty.error(getApplicationContext(), invalidEmailMsg, Toasty.LENGTH_SHORT, true).show();
                     return;
                 }
-                if(currentUser.getBoolean("barber")) {
+                if(currentUser.getBoolean(barberKey)) {
                     if (etPrice.getText().toString().isEmpty()) {
-                        Toasty.error(getApplicationContext(), "First input a price", Toasty.LENGTH_SHORT, true).show();
+                        Toasty.error(getApplicationContext(), invalidPriceMsg, Toasty.LENGTH_SHORT, true).show();
                         return;
                     }
                 }
-                if(currentUser.getParseGeoPoint("mapPoint") == null){
-                    Toasty.error(getApplicationContext(), "First input a postal address!", Toasty.LENGTH_SHORT, true).show();
+                if(currentUser.getParseGeoPoint(geokey) == null){
+                    Toasty.error(getApplicationContext(), noAddressMsg, Toasty.LENGTH_SHORT, true).show();
                     return;
                 }
-                if(!cbHaircut.isChecked() && !cbBeard.isChecked() && !cbColor.isChecked() && !cbBlowdry.isChecked() && !cbWax.isChecked()){
-                    Toasty.error(getApplicationContext(), "Must Select atleast one service!", Toasty.LENGTH_SHORT, true).show();
+                if(!(cbHaircut.isChecked()) && !(cbBeard.isChecked()) && !(cbColor.isChecked()) && !(cbBlowdry.isChecked()) && !(cbWax.isChecked())){
+                    Toasty.error(getApplicationContext(), serviceMsg, Toasty.LENGTH_SHORT, true).show();
                     return;
                 }
+                currentUser.put(emailKey, etEmail.getText().toString());
+                currentUser.put(priceKey, Integer.valueOf(etPrice.getText().toString()));
+                serviceChanges(view);
+                currentUser.saveInBackground();
+                goMainActivity();
             }
         });
 
@@ -88,15 +111,43 @@ public class SignupActivity extends AppCompatActivity {
         startActivityForResult(googleMaps, requestCodeNum);
     }
 
+    private void goMainActivity() {
+        Intent goToMain  = new Intent(this, MainActivity.class);
+        startActivity(goToMain);
+        Toasty.success(SignupActivity.this, signupSuccess, Toasty.LENGTH_SHORT, true).show();
+        finish();
+    }
+
     @Override
     public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == requestCodeNum && resultCode == RESULT_OK) {
             String result = data.getStringExtra(addressKey);
-            tvAddress.setText(result);
-            myself.put(addressKey, result);
-            myself.saveInBackground();
+            btnAddress.setText(result);
+            currentUser.put(addressKey, result);
+            currentUser.saveInBackground();
         }
+        if (requestCode == requestCodeNum && resultCode == RESULT_OK) {
+            String result = data.getStringExtra(addressKey);
+            btnAddress.setText(result);
+            currentUser.put(addressKey, result);
+            currentUser.saveInBackground();
+        }
+    }
 
+    public void serviceChanges(View v){
+        // go through each service checkbox, and if its checked, add it to the serviceList of offered services
+        List<String> serviceList = new ArrayList<String>();
+        if(cbHaircut.isChecked())
+            serviceList.add(String.valueOf(cbHaircut.getText()));
+        if(cbBeard.isChecked())
+            serviceList.add(String.valueOf(cbBeard.getText()));
+        if(cbColor.isChecked())
+            serviceList.add(String.valueOf(cbColor.getText()));
+        if(cbWax.isChecked())
+            serviceList.add(String.valueOf(cbWax.getText()));
+        if(cbBlowdry.isChecked())
+            serviceList.add(String.valueOf(cbBlowdry.getText()));
+        currentUser.put(servicesKey, serviceList);
+        currentUser.saveInBackground();
     }
 }
